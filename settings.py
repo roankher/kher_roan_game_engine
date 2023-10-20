e5922aade461f78c0872b225b951d101fc8a7eb3
roankher <31666810+roankher@users.noreply.github.com>
2023-10-20 09:26:11 -0700

    First push

---
+++ settings.py
@@ -0,0 +1,24 @@
+# This file was created by Roan Kher
+
+# game settings 
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+SCORE = 0
+
+# player settings
+PLAYER_JUMP = 50
+PLAYER_GRAV = 1
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+PLATFORM_LIST = [0, HEIGHT - 40, WIDTH, 40, "normal"],
+(WIDTH/2 - 50, HEIGHT * 3 / 4, 100, 20, "normal"),
+(125, HEIGHT - 350, 100, 20, "normal"),
+(350, 200, 100, 20, "normal")
+(175, 100, 50, 20, "normal")
